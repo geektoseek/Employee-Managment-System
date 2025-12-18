f7f3ed3658e93bc843c588f969b580290356709d
naeem <naeeminfo120@gmail.com>
2025-12-18 18:36:11 +0500

    Task Data Loading Auto From Local Storage

--- src/Components/TaskList/AcceptTask.jsx
+++ src/Components/TaskList/AcceptTask.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import { data } from 'react-router-dom'
 
-const AcceptTask = () => {
+const AcceptTask = ({ data }) => {
     return (
         <div>
             < div className='h-full w-[400px] bg-gray-600 border-[1] border-gray-400 rounded-2xl shrink-0 p-5' >
                 <div className='flex items-center justify-between'>
-                    <h3 className='bg-red-600 px-4 py-2 rounded-sm text-white'>High</h3>
-                    <h4 className='text-white'>24 Feb 2025</h4>
+                    <h3 className='bg-red-600 px-4 py-2 rounded-sm text-white'>{data.category}</h3>
+                    <h4 className='text-white'>{data.date}</h4>
                 </div>
-                <h2 className='text-3xl text-white mt-10 font-bold'>I have accepted this task Make a HR management System</h2>
-                <p className='text-white mt-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur recusandae tempora quibusdam labore. Sint temporibus autem delectus deleniti dolorum accusamus.</p>
+                <h2 className='text-3xl text-white mt-10 font-bold'>{data.title}</h2>
+                <p className='text-white mt-5'>{data.description}</p>
                 <div className='flex items-center justify-between w-full mt-4'>
                     <button className='bg-emerald-600 py-2 w-[45%] font-semibold px-3 text-white text-sm rounded'>Mark as Completed</button>
                     <button className='bg-red-500 py-2 px-3 w-[45%] font-semibold text-white text-sm rounded'>Mark as Failed</button>
--- src/Components/TaskList/CompletedTask.jsx
+++ src/Components/TaskList/CompletedTask.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 
-const CompletedTask = () => {
+const CompletedTask = ({ data }) => {
     return (
         <div>
 
             < div className='h-full w-[400px] bg-green-950 border border-gray-400 rounded-2xl shrink-0 p-5' >
                 <div className='flex items-center justify-between'>
-                    <h3 className='bg-red-600 px-4 py-2 rounded-sm text-white'>High</h3>
-                    <h4 className='text-white'>24 Feb 2025</h4>
+                    <h3 className='bg-red-600 px-4 py-2 rounded-sm text-white'>{data.category}</h3>
+                    <h4 className='text-white'>{data.date}</h4>
                 </div>
-                <h2 className='text-3xl text-white mt-10 font-bold'>I have completed this task Make a HR management System</h2>
-                <p className='text-white mt-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur recusandae tempora quibusdam labore. Sint temporibus autem delectus deleniti dolorum accusamus.</p>
+                <h2 className='text-3xl text-white mt-10 font-bold'>{data.title}</h2>
+                <p className='text-white mt-5'>{data.description}</p>
                 <div className='mt-4'>
                     <button className='bg-green-500 rounded w-full py-2 px-4 text-white text-sm'>Completed</button>
                 </div>
